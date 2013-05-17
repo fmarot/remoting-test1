fdf796ab2db2dfe05bd785ea8e5be45d3ea557c1
Francois Marot <Francois.Marot@olea-medical.com>
2013-05-17 15:11:34 +0200

    updated .gitignore + added missing java file

---
+++ images-client/src/main/java/com/teamtter/ClientApp.java
@@ -0,0 +1,31 @@
+package com.teamtter;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.teamtter.config.AppSpringConfig;
+import com.teamtter.data.ImageServiceMetaData;
+import com.teamtter.services.IImageService;
+
+public class ClientApp {
+
+	private static final Logger	log	= LoggerFactory.getLogger(ClientApp.class);
+
+	private void start() throws Exception {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppSpringConfig.class);
+		Map<String, IImageService> beansOfType = context.getBeansOfType(IImageService.class);
+		for (IImageService service : beansOfType.values()) {
+			ImageServiceMetaData serviceMetaData = service.getServiceMetaData();
+			log.warn("Service metaData: {}", serviceMetaData);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		ClientApp clientApp = new ClientApp();
+		clientApp.start();
+	}
+
+}
